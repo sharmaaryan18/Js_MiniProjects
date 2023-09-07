5c59719c0ebb5356b5222624b04e3bf33be09fae
sharmaaryan18 <aryansharm3876@gmail.com>
2023-09-07 20:11:38 +0530

    PasswordGenerator

--- index.js
+++ index.js
@@ -10,4 +10,98 @@ const symbolsCheck =document.querySelector("#symbols");
 const indicator =document.querySelector("[data-indicator]");
 const generateBtn =document.querySelector(".generateButton");
 const allCheckBox =document.querySelector("input[type=checkbox]"); 
+const symbols = '!@~`#$%^&*(){}:?/";';
+
+
+let password="";
+let passwordLength = 15;
+let checkCount =1;
+
+handleSlider();
+// Set the length of Password
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText =passwordLength;
+}
+
+function setIndicator(color) {
+    indicator.style.background= color;
+    //shadow
+}
+
+function getRndInteger(min,max) {
+     return Math.floor(Math.random()*(max-min))+min//will give the int from Min to Max
+}
+
+function generateRandomNumber() {
+    return getRndInteger(0,9);
+}
+
+function generateLowerCase() {
+    return String.fromCharCode(getRndInteger(97,123))
+}
+
+function generateUpperCase() {
+    return String.fromCharCode(getRndInteger(65,91))
+}
+
+function generateSymbol() {
+    const randNum = getRndInteger(0,symbols.length);
+    return symbols.charAt(randNum);
+}
+
+function calcStrength() {
+    let hasUpper= false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+
+    if(uppercaseCheck.checked) hasUpper = true;
+    if(lowercaseCheck.checked) hasLower = true;
+    if(numbersCheck.checked) hasNum = true;
+    if(symbolsCheck.checked) hasSym = true;
+
+    if(hasUpper && hasLower && (hasNum||hasSym) && passwordLength>=8) {
+        setIndicator("#0f0");
+    } else if(
+        (hasUpper || hasLower) &&
+        (hasNum || hasSym) &&
+        passwordLength>=6
+    ) {
+        setIndicator("#ff0");
+    }
+    else {
+        setIndicator("#f00");
+    }
+}
+
+ async function copyContent() {
+     try{
+   await navigator.clipboard.writeText(passWordDisplay.value);
+   copyMsg.innerText="copied";
+     }
+     catch(e) {
+        copyMsg.innerText="Failed";
+     }
+     //To make copy wala span visible
+     copyMsg.classList.add("active");
+
+     setTimeout(()=>{
+        copyMsg.classList.remove("active");
+     },2000);
+}
+
+inputSlider.addEventListener('input', function (e) {
+        passwordLength = e.target.value;
+        handleSlider();
+    })
+
+copyBtn.addEventListener('click', ()=>{
+    if(passWordDisplay.value)
+    copyContent(); 
+})
+
+
+ 
+ 
 
